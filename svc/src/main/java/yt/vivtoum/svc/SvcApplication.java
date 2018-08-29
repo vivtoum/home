bdb727e4cc530746d8d97a20673eaabc003ca726
vivtoum <vedyou@qq.com>
2018-08-29 21:18:58 +0800

    分布式demo

---
+++ svc/src/main/java/yt/vivtoum/svc/SvcApplication.java
@@ -0,0 +1,12 @@
+package yt.vivtoum.svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SvcApplication.class, args);
+    }
+}
---
+++ svc/src/test/java/yt/vivtoum/svc/SvcApplicationTests.java
@@ -0,0 +1,16 @@
+package yt.vivtoum.svc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SvcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
